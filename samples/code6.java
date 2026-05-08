f3a2afd23fd2f5010069900c13dfd5e2054eb074
Luis Callejas <jake784@Luiss-MacBook-Air.local>
2026-05-08 10:00:35 -0600

    feat: more samples to get lexical errors in console

---
+++ samples/code6.java
@@ -0,0 +1,31 @@
+public class PruebaDeFuego {
+    public static void main(String[] args) {
+        // 1. Arranque limpio y válido
+        int limite = 100;
+        int contador = 0;
+
+        // 2. ERRORES LÉXICOS: Caracteres inválidos (@ y #)
+        // El Scanner debe atraparlos en su tabla de errores léxicos.
+        int @variableRota = 10;
+        contador = contador + #5;
+
+        // 3. ERROR SINTÁCTICO: Falta el punto y coma (;)
+        // El Parser debe quejarse, pero el Scanner lo leerá bien.
+        int saltoDeLinea = 20
+        
+        // 4. RECUPERACIÓN 1: El AST debe lograr dibujar esto a pesar del caos de arriba
+        int puntoDeControl = 1;
+
+        // 5. ERROR SINTÁCTICO: Falta cerrar el paréntesis de la condición
+        while (contador < limite {
+            contador = contador + 10;
+        }
+
+        // 6. COMBO (LÉXICO + SINTÁCTICO): Símbolo extraño (~) y operador colgante (+)
+        int locuraTotal = 50 + ~ ;
+
+        // 7. RECUPERACIÓN FINAL: Si el nodo de esta variable aparece en tu gráfico interactivo, 
+        // tu compilador es oficialmente indestructible.
+        boolean compiladorSobrevive = true;
+    }
+}
